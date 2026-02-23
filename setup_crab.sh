b4409c21e9c5b6c9329c51b4cead2aa1c8c29b97
Robert Ward <robert.james.ward@cern.ch>
2026-02-23 15:35:39 +0100

    Update crab setup script to check if a voms proxy exists already

--- setup_crab.sh
+++ setup_crab.sh
@@ -1,3 +1,11 @@
 # eval `scram runtime -sh`
 # source /cvmfs/cms.cern.ch/crab3/crab.sh
-voms-proxy-init --voms cms --valid 168:00
+# voms-proxy-init --voms cms --valid 168:00
+
+# Check if a valid VOMS proxy exists
+if ! voms-proxy-info --exists &>/dev/null; then
+    echo "No valid VOMS proxy, setting one up"
+    voms-proxy-init --voms cms --valid 168:00
+else
+    echo "VOMS Proxy found!"
+fi
