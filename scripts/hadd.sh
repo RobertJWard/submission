cd2b2b8988f163bd6a6859fa9a8ddd6e89e4b926
Robert Ward <robert.james.ward@cern.ch>
2026-05-30 23:10:15 +0200

    Move hadding logic to separate function to clean jobs output

--- scripts/hadd.sh
+++ scripts/hadd.sh
@@ -9,8 +9,7 @@
 # INPUTS="170pre1"
 INPUTS="170pre1_MuonOMTF 161pre4 161pre3"
 
-
-REVISION="260518"
+REVISION=$(date +%y%m%d)
 
 TEMP=$(mktemp -d)
 echo "Temp directory is $TEMP"
@@ -18,30 +17,33 @@ echo "Temp directory is $TEMP"
 MAX_JOBS=12
 PARENTDIR="/eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/roward/phase2/menu/ntuples/Spring24"
 
+haddFiles() {
+    local sample="$1"
+    local temp=${sample#*v45/}
+    local sampleShort=${temp%%_TuneCP5*}
+    local tempPU=${sample#*Spring24_}
+    local samplePU=${tempPU%%_V45*}
+    echo -e "Sample: $sampleShort\nPU: $samplePU\nDirectory: $sample" |& tee -a logs/hadd_${INPUT}_${REVISION}.log
+    local hadddir=${sample/0000/hadd}
+    if [ -d $hadddir ]; then
+        echo -e "Skipping as hadd directory already exists - check if this is expected\nHadddir: $hadddir" |& tee -a logs/hadd_${INPUT}_${REVISION}.log
+    else
+        mkdir $hadddir
+        # { time hadd -n 5 -j 12 -d $TEMP $TEMP/output_Phase2_L1T.root $sample/output_*.root; } |& tee -a logs/hadd_${INPUT}_${REVISION}.log
+        { time hadd -fk -d $TEMP $TEMP/output_Phase2_L1T_${INPUT}_${sampleShort}_${samplePU}.root $sample/output_*.root; } >> logs/hadd_${INPUT}_${REVISION}.log
+        mv $TEMP/output_Phase2_L1T_${INPUT}_${sampleShort}_${samplePU}.root $hadddir/output_Phase2_L1T.root
+        echo "Hadd complete: $INPUT $sampleShort $samplePU" |& tee -a logs/hadd_${INPUT}_${REVISION}.log
+        echo "Hadd output: $hadddir" |& tee -a logs/hadd_${INPUT}_${REVISION}.log
+    fi
+}
+
 for INPUT in $INPUTS; do
     echo "INPUT: $INPUT" |& tee logs/hadd_${INPUT}_${REVISION}.log
     for sample in $(find "$PARENTDIR/$INPUT/v45/" -wholename "*_TuneCP5*/0000" ! -wholename "*LongLived*"); do
-	while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
-	    sleep 3
-	done
-	(
-	    temp=${sample#*v45/}  # Remove everything up to and including "v45/"
-	    sampleShort=${temp%%_TuneCP5*}  # Remove "_TuneCP5" and everything after
-	    tempPU=${sample#*Spring24_} # Remove everything up to and including "Spring24_"
-	    samplePU=${tempPU%%_V45*} # Remove "_V45" and everything after
-	    echo -e "Sample: $sampleShort\nPU: $samplePU\nDirectory: $sample" |& tee -a logs/hadd_${INPUT}_${REVISION}.log
-	    hadddir=${sample/0000/hadd}
-	    if [ -d $hadddir ]; then
-		echo -e "Skipping as hadd directory already exists - check if this is expected\nHadddir: $hadddir"  |& tee -a logs/hadd_${INPUT}_${REVISION}.log
-	    else
-		mkdir $hadddir
-		# { time hadd -n 5 -j 12 -d $TEMP $TEMP/output_Phase2_L1T.root $sample/output_*.root; } |& tee -a logs/hadd_${INPUT}_${REVISION}.log
-		{ time hadd -fk -d $TEMP $TEMP/output_Phase2_L1T_${INPUT}_${sampleShort}_${samplePU}.root $sample/output_*.root; } >> logs/hadd_${INPUT}_${REVISION}.log
-		mv $TEMP/output_Phase2_L1T_${INPUT}_${sampleShort}_${samplePU}.root $hadddir/output_Phase2_L1T.root
-		echo "Hadd complete: $INPUT $sampleShort $samplePU"  |& tee -a logs/hadd_${INPUT}_${REVISION}.log
-		echo "Hadd output: $hadddir"  |& tee -a logs/hadd_${INPUT}_${REVISION}.log
-	    fi
-	) &
+        while [ $(jobs -p | wc -l) -ge $MAX_JOBS ]; do
+            sleep 3
+        done
+        haddFiles "$sample" &
     done
 done
 
