9ddd50723638c434ca0b8533f7caf719d3dfb8d3
Robert Ward <robert.james.ward@cern.ch>
2026-09-11 16:43:34 +0200

    Convenience script to find hadded file folders

---
+++ scripts/haddFolderFinder.sh
@@ -0,0 +1,8 @@
+DATASET=200_D121
+# DATASET=200pre1_RelVal
+# DATASET=200pre1_MuonShower
+# DATASET=170pre1_NGJetModel
+# DATASET=161pre4_CorrEmu
+# DATASET=161pre4_MuonGMT
+
+find "/eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/roward/phase2/menu/ntuples/RelVal/$DATASET/v45/" -name "hadd" # | xargs ls
