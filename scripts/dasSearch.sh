67f8210dd1eaf5955efd52e703d42aefefc84e57
Robert Ward <robert.james.ward@cern.ch>
2026-09-11 16:41:52 +0200

    Minor changes to das scripts

--- scripts/dasSearch.sh
+++ scripts/dasSearch.sh
@@ -1,5 +1,6 @@
 # dasgoclient -query="dataset=/HTo2LongLivedTo2mu2jet*/*Spring24*/GEN-SIM-DIGI-RAW-MINIAOD" |& tee logs/HTo2LongLivedTo2mu2jet.log
 # dasgoclient -query="dataset=/HTo2LongLivedTo4mu*/*Spring24*/GEN-SIM-DIGI-RAW-MINIAOD" |& tee logs/HTo2LongLivedTo4mu.log
 
-dasgoclient -query="dataset=/*14TeV*/*Phase2Spring24*PU*/GEN-SIM-DIGI-RAW-MINIAOD" |& tee logs/phase2.log
+# dasgoclient -query="dataset=/*14TeV*/*Phase2Spring24*PU*/GEN-SIM-DIGI-RAW-MINIAOD" |& tee logs/phase2.log
+dasgoclient -query="dataset=/*13p6TeV*/*RunIII2024Summer24*NanoAODv15*/NANOAODSIM" |& tee logs/phase1.log
 
--- scripts/relValDas.sh
+++ scripts/relValDas.sh
@@ -1,1 +1,3 @@
-dasgoclient -query="dataset=/RelVal*/CMSSW_20_0_0_pre1*/GEN-SIM-DIGI-RAW*"
+# dasgoclient -query="dataset=/RelVal*/CMSSW_20_0_0_pre1*/GEN-SIM-DIGI-RAW*"
+dasgoclient -query="dataset=/RelVal*/CMSSW_20_0_0*/GEN-SIM-DIGI-RAW*"
+# dasgoclient -query="dataset=/RelVal*/CMSSW_20_0_0_patch1-150X_mcRun4_realistic_v1_STD_D12*_RecycledGEN_noPU_*/GEN-SIM-DIGI-RAW*"
